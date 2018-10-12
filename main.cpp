a6cce4df60f35720cb461f15492727b989a20e1b
Unknown <esrotica@gmail.com>
2018-10-12 16:27:50 -0400

    Commit 02

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <yaml-cpp/yaml.h>
+#include <yaml-cpp/exceptions.h>
+#include <yaml-cpp/node/impl.h>
+#include <nlohmann/json.hpp>
+#include <inja.hpp>
+#include <regex>
+
+using json = nlohmann::json;
+using namespace inja;
+
+
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
