efa824e032485ac6b621b2ca4ddbb4b093d33c01
Unknown <esrotica@gmail.com>
2018-12-09 17:18:40 -0500

    Commit 37a

---
+++ YAMLtoJSON.h
@@ -0,0 +1,14 @@
+//
+// Created by jwc on 12/9/18.
+//
+
+#ifndef FS2_MIGRATION01_YAMLTOJSON_H
+#define FS2_MIGRATION01_YAMLTOJSON_H
+
+
+class YAMLtoJSON {
+
+};
+
+
+#endif //FS2_MIGRATION01_YAMLTOJSON_H
